7c2700e2fd479a80878eca5327165e40f9ba24fa
Harsh Kalra <97459860+harshkalra2003@users.noreply.github.com>
2023-09-08 00:37:41 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const { space } = require('postcss/lib/list');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        "lg":"480px"  
+      },
+      spacing:{
+        "big":"35rem"
+      }
+    },
+    fontFamily:{
+      nunito:['Nunito','sans-serif']
+    }
   },
   plugins: [],
 }
